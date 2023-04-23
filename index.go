e2a815b94ea25da5841ec199fe302f4b2b79c0e3
idontknowtoobrother <idontknowbrotherpls@gmail.com>
2023-04-23 20:13:56 +0700

    make() arrays with min and max of size, added: debug func show len cap and value

--- index.go
+++ index.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var numbers = make([]int, 5, 10)
+	statusOfArrays(numbers)
+}
+
+func statusOfArrays(arr []int) {
+	fmt.Printf("len: %d, cap: %d, value: %v", len(arr), cap(arr), arr)
 }
