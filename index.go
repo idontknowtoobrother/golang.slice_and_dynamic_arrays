e153bb83ae93feada2b6807a6568f723ecf21d38
idontknowtoobrother <idontknowbrotherpls@gmail.com>
2023-04-23 20:17:03 +0700

    dynamic arrays

--- index.go
+++ index.go
@@ -5,8 +5,13 @@ import "fmt"
 func main() {
 	var numbers = make([]int, 5, 10)
 	statusOfArrays(numbers)
+
+	var dynamicNumbers []int
+	dynamicNumbers = append(dynamicNumbers, 1)
+	dynamicNumbers = append(dynamicNumbers, 12)
+	statusOfArrays(dynamicNumbers)
 }
 
 func statusOfArrays(arr []int) {
-	fmt.Printf("len: %d, cap: %d, value: %v", len(arr), cap(arr), arr)
+	fmt.Printf("\nlen: %d, cap: %d, value: %v", len(arr), cap(arr), arr)
 }
